e63b965c724d4a6f5c9c21fd783bca552ce277d7
Vidul <your_email@example.comvidulsathmika@gmail.com>
2026-06-12 06:12:50 +0530

    databaseupdate

--- backend/models/CallLog.js
+++ backend/models/CallLog.js
@@ -3,13 +3,11 @@ import { secondaryConnection } from '../config.js';
 
 const CallLogSchema = new Schema({
   whatsappNumber: { type: String, required: true, unique: true },
-  callTaken: { type: Boolean, default: true },
-  calledAt: { type: Date, default: Date.now },
-  participationConfirmed: { type: Boolean, default: null },
-  firstName: { type: String },
-  lastName: { type: String },
-  examCenter: { type: String },
+  name: { type: String },
   nic: { type: String },
+  participationConfirmed: { type: Boolean, required: true },
+  calledAt: { type: Date, default: Date.now },
+  examCenter: { type: String },
   studentId: { type: String }
 }, { timestamps: true });
 
